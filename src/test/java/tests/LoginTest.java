91586402646ad8fa2f39f7344543446e76114bae
Ross <Admin>
2022-07-06 23:36:50 +0200

    Added user test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,13 +3,13 @@ package tests;
 import model.LoginRequestModel;
 import model.LoginResponseModel;
 import org.testng.annotations.Test;
+import util.Config;
 import util.UserGenerator;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertNotNull;
 import static steps.LoginSteps.postLogin;
-import static util.Config.addProperty;
-import static util.Config.getProperty;
 
 public class LoginTest {
 
@@ -18,10 +18,25 @@ public class LoginTest {
         LoginRequestModel loginRequestModel = UserGenerator.createLoginRequestModel();
         LoginResponseModel loginResponse = postLogin(loginRequestModel);
 
-        assertThat(loginResponse.getEmail()).isEqualTo(getProperty("email"));
+        assertThat(loginResponse.getEmail()).isEqualTo(Config.getProperty("email"));
         assertNotNull(loginResponse.getToken());
         assertThat(loginResponse.getEmail()).isEqualTo(loginRequestModel.getEmail());
 
-        addProperty("token",loginResponse.getToken()); // Setting token property for future use in tests
+        Config.addProperty("token",loginResponse.getToken()); // Setting token property for future use in tests
+        Config.addProperty("userId",loginResponse.getUserId()); // Setting userId property for future use in tests
+    }
+
+    @Test(description = "GET /user/{id} -> check 200 status")
+    public void dummy() {
+        given()
+                .baseUri(Config.getProperty("baseUri"))
+                .basePath("/users/" +Config.getProperty("userId"))
+                .contentType("application/json")
+                .header("Authorization",Config.getProperty("token"))
+                .log().all()
+                .when()
+                .get()
+                .then()
+                .statusCode(200);
     }
 }
